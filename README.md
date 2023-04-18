31802f938fdcd61bfa2288df8f6fed15741e34a1
juliakaplan98 <juliakaplan98@outlook.com>
2023-04-17 23:47:56 -0400

    Context menu

--- README.md
+++ README.md
@@ -29,3 +29,73 @@ pip install openpyxl
 
 https://xlsxwriter.readthedocs.io/index.html#
 pip install XlsxWriter
+
+# Below are quick example
+
+# Append Row to DataFrame
+
+list_row = ["Hyperion", 27000, "60days", 2000]
+df.loc[len(df)] = list_row
+
+# Using pandas.concat() to append a row
+
+new_row = pd.DataFrame({'Courses':'Hyperion', 'Fee':24000, 'Duration':'55days', 'Discount':1800}, index=[0])
+df2 = pd.concat([new_row,df.loc[:]]).reset_index(drop=True)
+
+# Append specific row/index name using DataFrame.loc[]
+
+df.loc['7', :] = ['Hive',25000,'45days',2000]
+
+# Append row in DataFrame using DataFrame.loc[]
+
+df.loc['7'] = ['Hive',25000,'45days',2000]
+
+# New list to append Row to DataFrame
+
+list = ["Hyperion", 27000, "60days", 2000]
+df.loc[len(df)] = list
+print(df)
+
+# Using pandas.concat() to append a row
+
+new_row = pd.DataFrame({'Courses':'Hyperion', 'Fee':24000, 'Duration':'55days', 'Discount':1800}, index=[0])
+df2 = pd.concat([new_row,df.loc[:]]).reset_index(drop=True)
+print (df2)
+
+# Append specific row/index name using DataFrame.loc[]
+
+df.loc['7', :] = ['Hive',25000,'45days',2000]
+print(df)
+
+# Append row in DataFrame using DataFrame.loc[]
+
+df.loc['7'] = ['Hive',25000,'45days',2000]
+print(df)
+
+import pandas as pd
+technologies = ({
+'Courses':["Spark","Hadoop","pandas","Java","Pyspark"],
+'Fee' :[20000,25000,30000,22000,26000],
+'Duration':['30days','40days','35days','60days','50days'],
+'Discount':[1000,2500,1500,1200,3000]
+})
+df = pd.DataFrame(technologies)
+print(df)
+
+# Using pandas.concat() to add a row
+
+new_row = pd.DataFrame({'Courses':'Hyperion', 'Fee':24000, 'Duration':'55days', 'Discount':1800}, index=[0])
+df2 = pd.concat([new_row, df.loc[:]]).reset_index(drop=True)
+print (df2)
+
+# Add specific row/index name using DataFrame.loc[]
+
+df.loc['Index5', :] = ['Hive',25000,'45days',2000]
+print(df)
+
+# Add row in DataFrame using DataFrame.loc[]
+
+df.loc['Index5'] = ['Hive',25000,'45days',2000]
+print(df)
+
+Sumire Matsubara
--- pdfmanipulator/tables/table_widget.py
+++ pdfmanipulator/tables/table_widget.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import math
 from PyQt6 import QtCore
-from PyQt6.QtWidgets import QTableView
+from PyQt6.QtWidgets import QTableView, QMenu
 from PyQt6.QtCore import Qt, QModelIndex
+from PyQt6.QtGui import QAction
 
 from .table_model import TableModel
 
@@ -16,3 +17,102 @@ class TableWidget(QTableView):
         self.rows = self.model.rows
         self.setModel(self.model)
         self.setMinimumHeight(500)
+        self.createActions()
+
+    def contextMenuEvent(self, event):
+        """Create context menu and additional actions."""
+        a = self.selectedIndexes()[0]
+        c = a.column()
+        v = a.row()
+        context_menu = QMenu(self)
+        context_menu.addAction(self.add_row_above_act)
+        context_menu.addAction(self.add_row_below_act)
+        context_menu.addSeparator()
+        context_menu.addAction(self.add_col_before_act)
+        context_menu.addAction(self.add_col_after_act)
+        context_menu.addSeparator()
+        context_menu.addAction(self.delete_row_act)
+        context_menu.addAction(self.delete_col_act)
+        context_menu.addSeparator()
+        # Create actions specific to the context menu
+        copy_act = context_menu.addAction("Copy")
+        paste_act = context_menu.addAction("Paste")
+        context_menu.addSeparator()
+        context_menu.addAction(self.clear_table_act)
+        # Execute the context_menu and return the action
+        # selected. mapToGlobal() translates the position
+        # of the window coordinates to the global screen
+        # coordinates. This way we can detect if a right-click
+        # occurred inside of the GUI and display the context
+        # menu
+        action = context_menu.exec(self.mapToGlobal(event.pos()))
+        # Check for actions selected in the context menu that
+        # were not created in the menu bar
+        if action == copy_act:
+            self.copyItem()
+        if action == paste_act:
+            self.pasteItem()
+
+    def createActions(self):
+        """Create the application's menu actions."""
+        # Create actions for File menu
+        self.quit_act = QAction("Quit", self)
+        self.quit_act.setShortcut("Ctrl+Q")
+        self.quit_act.triggered.connect(self.close)
+        # Create actions for Table menu
+        self.add_row_above_act = QAction("Add Row Above", self)
+        self.add_row_above_act.triggered.connect(self.addRowAbove)
+        self.add_row_below_act = QAction("Add Row Below", self)
+        self.add_row_below_act.triggered.connect(self.addRowBelow)
+        self.add_col_before_act = QAction("Add Column Before", self)
+        self.add_col_before_act.triggered.connect(self.addColumnBefore)
+        self.add_col_after_act = QAction("Add Column After", self)
+        self.add_col_after_act.triggered.connect(self.addColumnAfter)
+        self.delete_row_act = QAction("Delete Row", self)
+        self.delete_row_act.triggered.connect(self.deleteRow)
+        self.delete_col_act = QAction("Delete Column", self)
+        self.delete_col_act.triggered.connect(self.deleteColumn)
+        self.clear_table_act = QAction("Clear All", self)
+        self.clear_table_act.triggered.connect(self.clearTable)
+
+    def addRowAbove(self):
+        values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        header = [col for col in self.data_frame.head().columns]
+        line = pd.DataFrame(
+            {h: v for h, v in zip(header, values)},
+            index=[0],
+        )
+        self.data_frame = pd.concat([line, self.data_frame[:]]).reset_index(
+            drop=True
+        )
+
+        #  self.data_frame.loc[len(self.data_frame)] = ["1", "2", "3", "4"]
+        self.model = TableModel(self.data_frame)
+        # self.rows = self.model.rows
+        self.setModel(self.model)
+
+        # current_row = self.table_widget.currentRow()
+        # self.insertRow(current_row)
+
+    def addRowBelow(self):
+        current_row = self.table_widget.currentRow()
+        self.table_widget.insertRow(current_row + 1)
+
+    def addColumnBefore(self):
+        current_col = self.table_widget.currentColumn()
+        self.table_widget.insertColumn(current_col)
+
+    def addColumnAfter(self):
+        current_col = self.table_widget.currentColumn()
+        self.table_widget.insertColumn(current_col + 1)
+
+    def deleteRow(self):
+        current_row = self.table_widget.currentRow()
+        self.table_widget.removeRow(current_row)
+
+    def deleteColumn(self):
+        current_col = self.table_widget.currentColumn()
+        self.table_widget.removeColumn(current_col)
+
+    def clearTable(self):
+        self.table_widget.clear()
