c175cf7212871d93c5698b3811e724f2d55af780
juliakaplan98 <juliakaplan98@outlook.com>
2023-04-22 18:43:12 -0400

    Clean up

--- pdfmanipulator/data_model/copy_past.py
+++ pdfmanipulator/data_model/copy_past.py
@@ -1,17 +1,32 @@
-from typing import Set
 from typing import List
 from typing import Any
 
 
 class CopyPast:
     __copied_rows: List[List[Any]] = None
+    __copied_columns: List[List[Any]] = None
+    __copied_rectangle: List[List[Any]] = None
 
     @classmethod
-    @property
-    def copied_rows(cls) -> List[List[Any]]:
+    def get_copied_rows(cls) -> List[List[Any]]:
         return cls.__copied_rows
 
     @classmethod
-    @copied_rows.setter
-    def copied_rows(cls, copied_lines: List[List[Any]]):
-        __copied_rows = copied_lines
+    def set_copied_rows(cls, copied_lines: List[List[Any]]):
+        cls.__copied_rows = copied_lines
+
+    @classmethod
+    def get_copied_columns(cls) -> List[List[Any]]:
+        return cls.__copied_columns
+
+    @classmethod
+    def set_copied_columns(cls, copied_columns: List[List[Any]]):
+        cls.__copied_columns = copied_columns
+
+    @classmethod
+    def get_copied_rectangle(cls) -> List[List[Any]]:
+        return cls.__copied_rectangle
+
+    @classmethod
+    def set_copied_rectangle(cls, copied_rectangle: List[List[Any]]):
+        cls.__copied_rectangle = copied_rectangle
